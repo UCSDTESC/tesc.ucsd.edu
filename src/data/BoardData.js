ad6fed6ed200dbedc64a67f2c117a7f9fe0e6477
Subhankar Panda <s1panda@ucsd.edu>
2018-11-03 21:30:48 -0700

    Added Almost All Board Images

--- src/data/BoardData.js
+++ src/data/BoardData.js
@@ -2,82 +2,82 @@ const board = [
     {
         name: 'Colin Feeney',
         role: 'President',
-        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/placeholder.png'
+        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/president.jpg'
     },
     {
         name: 'Caitlyn Liu',
         role: 'VP External',
-        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/placeholder.png'
+        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/vp-external.jpeg'
     },
     {
         name: 'Nick Thomson',
         role: 'VP Internal',
-        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/placeholder.png'
+        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/vp-internal.jpg'
     },
     {
         name: 'Callie Boskin',
         role: 'VP Finance',
-        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/placeholder.png'
+        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/vp-finance.jpeg'
     },
     {
         name: 'Jonathan Luck',
         role: 'VP Events',
-        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/placeholder.png'
+        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/vp-events.jpg'
     },
     {
         name: 'David Ding',
         role: 'Sponsorship Lead',
-        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/placeholder.png'
+        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/sponsorship-lead.png'
     },
     {
         name: 'Sergio Villazon',
         role: 'ProDev Lead',
-        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/placeholder.png'
+        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/prodev-lead.jpg'
     },
     {
         name: 'Bilguun Bulgan',
         role: 'HackXX Director',
-        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/placeholder.png'
+        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/hackxx-director-2.jpeg'
     },
     {
         name: 'Alice Lee',
         role: 'HackXX Director',
-        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/placeholder.png'
+        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/hackxx-director-1.jpg'
     },
     {
         name: 'Subhankar Panda',
         role: 'Technical Lead',
-        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/placeholder.png'
+        img: 'https://ca.slack-edge.com/T1598AR7T-U82KB5FG8-f5ea3a482fa2-1024'
     },
     {
         name: 'Cora Xing',
         role: 'Design Lead',
-        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/placeholder.png'
+        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/visualdesign-lead.jpg'
     },
     {
         name: 'Andy Li',
         role: 'Outreach Lead',
-        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/placeholder.png'
+        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/outreach-lead.jpg'
     },
     {
         name: 'Catherine Xia',
         role: 'Social Lead',
-        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/placeholder.png'
+        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/social-lead.jpg'
     },
     {
         name: 'Cameron Wilkinson',
         role: 'Council Relations Lead',
-        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/placeholder.png'
+        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/council-lead.JPG'
     },
     {
         name: 'Evon Hung',
         role: 'Marketing Lead',
-        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/placeholder.png'
+        img: 'https://ca.slack-edge.com/T1598AR7T-UC7DM47CM-55803746ac59-1024'
     },
     {
         name: 'Joshua Hedgpeth',
         role: 'Project Spaces Lead',
-        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/placeholder.png'
+        img: 'https://s3.us-east-2.amazonaws.com/tesc-website/board/projectspaces-lead.jpeg'
     },
     {
         name: 'Nick Lin',
--- src/pages/AboutPage/components/BoardMember.js
+++ src/pages/AboutPage/components/BoardMember.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import $ from 'jquery';
 
 class Board extends React.Component {
+
 
     render() {
         const {data} = this.props
 
         return (
             <div className="col-md-3 board__member">
-                <img src={data.img} className="board__member-img" />
+
+                <div src={data.img} className="board__member-img" style={{backgroundImage: `url(${data.img})`}}> </div>
                 <div className="text-center board__member-name">
                     {data.name}
                 </div>
