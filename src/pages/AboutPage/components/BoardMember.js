e01123b3763490eaede1ce1a59d98dc1d65476e8
Subhankar Panda <s1panda@ucsd.edu>
2018-11-04 01:10:45 -0700

    Hover State

--- src/pages/AboutPage/components/BoardMember.js
+++ src/pages/AboutPage/components/BoardMember.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import $ from 'jquery';
-
+import {findDOMNode} from 'react-dom';
 class Board extends React.Component {
+
+
 
 
     render() {
         const {data} = this.props
 
         return (
-            <div className="col-md-3 board__member">
-
+            <div className="col-md-3 board__member" >
                 <div src={data.img} className="board__member-img" style={{backgroundImage: `url(${data.img})`}}> </div>
                 <div className="text-center board__member-name">
                     {data.name}
