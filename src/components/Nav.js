f86c37887988ab8833ebcebcf779824c96489328
Subhankar Panda <s1panda@ucsd.edu>
2018-12-25 22:14:27 +0530

    Fixed Copy Inconsistency

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -31,7 +31,7 @@ class Nav extends React.Component {
                     <NavLink className="nav-link" to="/#contact">CONTACT</NavLink>
                 </li>
                 <li className="nav-item text-center">
-                    <NavLink className="nav-link btn nav__toggle" to="/orgs">For TESC Orgs</NavLink>
+                    <NavLink className="nav-link btn nav__toggle" to="/orgs">FOR TESC ORGS</NavLink>
                 </li>
             </ul>
         );
