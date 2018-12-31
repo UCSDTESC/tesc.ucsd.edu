3e92e286a19e37aa03220f7929d7781c74aa62c0
Jimmy Dang <immmjimmy@gmail.com>
2018-12-31 02:35:00 -0800

    Finished a rough draft of the hero design

--- src/pages/EnspirePage/components/Hero.js
+++ src/pages/EnspirePage/components/Hero.js
@@ -7,10 +7,20 @@ class Hero extends Component {
     render() {
 
         //write your css in src/scss/apps/enspire/components/_hero.scss
+        // temporarily removed <Nav /> because it was messing up with the layout
         return (
-            <section className="w-100 h-100 enspire-hero">
-                <Nav />
-                This is the Enspire Hero
+            <section className="w-100 h-100 d-flex align-items-center justify-content-center enspire-hero">
+                <div className="container-fluid text-center enspire-contents">
+                    <img src="/Enspire-logo-text-blue.svg" />
+                    <p>
+                        <br />
+                        UC San Diego &#9679; January 28, 2019
+                        <br />
+                        Explore UCSD for a day of fun and engaging engineering activities.
+                    </p>
+                    <button type="button" class="btn btn-outline-primary btn-custom-large btn-space-right">Register now</button>
+                    <button type="button" class="btn btn-outline-primary btn-custom-large btn-space-left">Volunteer sign-up</button>
+                </div>
             </section>
         )
     }
