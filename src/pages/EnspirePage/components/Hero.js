2bece4aa67c99f6d2179b84ed71c853331b2d577
immmjimmy <immmjimmy@gmail.com>
2019-01-03 01:02:23 -0800

    Removed random imports made by autocomplete

--- src/pages/EnspirePage/components/Hero.js
+++ src/pages/EnspirePage/components/Hero.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import $ from 'jquery';
-import { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } from 'constants';
 // import Nav from './Nav';
 
 class Hero extends Component {
