e0502ce09a0c4a0521af017d9c753574634d2f32
Lisa Luo <llluo@ucsd.edu>
2019-01-03 19:08:57 -0800

    Implemented main part of enspire-register. Still need to figure out fonts, colors, pic

--- src/pages/EnspirePage/components/Register.js
+++ src/pages/EnspirePage/components/Register.js
@@ -7,7 +7,66 @@ class Register extends Component {
         //write your css in src/scss/apps/enspire/components/_register.scss
         return(
             <div className="w-100 h-100 enspire-register">
-                This is the register page
+                <div className="enspire-register__head">
+                    <b>Register for Enspire!</b>
+                </div>
+
+                {/* 1x2 grid with words on left and picture on right */}
+                <div class="container">
+                    <div class="row">
+                        {/* For the words on the left */}
+                        <div class="col">
+                            <div className="enspire-register__left">
+                                <p class="text-left">Enspire is a free event and all students that come will be provided lunch and a t-shirt!</p>
+                                
+                                <div className="enspire-register__left-participants">
+                                    <ul class="list-unstyled">
+                                        <li>
+                                            <p><b>Participants,</b> please complete the following:</p>
+                                        </li>
+                                        <ul>
+                                            <li>
+                                                <p>
+                                                    <a href="https://www.google.com" target="_blank">liability waiver form</a> (one per student)
+                                                </p>
+                                                
+                                            </li>
+                                            <li>
+                                                <p>
+                                                    <a href="https://www.google.com" target="_blank">photo release form</a> (one per student)
+                                                </p>
+                                            </li>
+                                        </ul>
+                                    </ul>
+                                </div>
+
+                                <p className="enspire-register__note"><b>** Note: All students must have both waivers filled out to attend Enspire! **</b></p>
+                                
+                                <div className="enspire-register__left-volunteers">
+                                    <ul class="list-unstyled">
+                                        <li>
+                                            <p><b>Volunteers,</b> please complete the following:</p>
+                                        </li>
+                                        <ul>
+                                            <li>
+                                                <p>
+                                                    Fill out the <a href="https://www.google.com" target="_blank">volunteer registration form</a>
+                                                </p>
+                                            </li>
+                                        </ul>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div className="enspire-register__right">
+                                <img src="enspire-register-photo" class="float-right" alt="PLACEHOLDER"></img>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         )
     }
