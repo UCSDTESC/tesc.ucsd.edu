e7f6d80502524d90cdea8b4811f45819d66bbc58
Saurabh Kanhegaonkar <saurabh.kanhegaonkar@gmail.com>
2019-01-06 21:28:30 -0800

    Checkpoint - Adding data

--- src/pages/DecafPage/components/AttendingCompanies.js
+++ src/pages/DecafPage/components/AttendingCompanies.js
@@ -1,35 +1,76 @@
 import React from 'react';
 import {
-  Card, Button, CardImg, CardTitle, CardText, CardDeck,
+  Button, Badge, Card, CardImg, CardTitle, CardText, CardDeck,
   CardSubtitle, CardBody
 } from 'reactstrap';
+
+import Company from './Company';
 
 class AttendingCompanies extends React.Component {
 
   render() {
     return (
-      <div className="decaf-about" id="about">
-        <div className="decaf-about__head">
+      <div className="decaf-companies" id="about">
+        <div className="decaf-companies__head">
           Attending Companies
         </div>
-        <div className="decaf-about__container">
+        <div className="decaf-companies__container">
           <CardDeck>
-            <Card>
+            <Company name="Astrani" 
+                     positions={["Part-Time", "Internship", "Full-Time"]} 
+                     fields={["Aeronautical", "Mechanical", "Aeros"]}
+                     nationalities={["US Citizen", "International"]}
+                     description="Info about the cool company"
+            />
+            <Card className="decaf-company">
               <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-                <Button>Button</Button>
+                <CardTitle className="decaf-company__name">Astranis</CardTitle>
+                <CardSubtitle className="decaf-compay__position">Full-Time, Internship</CardSubtitle>
+                <div className="decaf-company__fields">
+                  <Badge className="decaf-company__field" color="info">Mechanical</Badge>
+                  <Badge className="decaf-company__field" color="info">Aerospace</Badge>
+                  <Badge className="decaf-company__field" color="info">Aerospace</Badge> 
+                </div>
+                <div className="decaf-company__nationalities">
+                  <Badge className="decaf-company__nationality" color="secondary">US Citizen</Badge>
+                  <Badge className="decaf-company__nationality" color="secondary">International</Badge>
+                </div>
+                <CardText cla>This is some text about the company</CardText>
               </CardBody>
             </Card>
-            <Card>
+            <Card className="decaf-company">
               <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-                <Button>Button</Button>
+                <CardTitle className="decaf-company__name">Astranis</CardTitle>
+                <CardSubtitle className="decaf-compay__position">Full-Time, Internship</CardSubtitle>
+                <div className="decaf-company__fields">
+                  <Badge className="decaf-company__field" color="info">Mechanical</Badge>
+                  <Badge className="decaf-company__field" color="info">Aerospace</Badge>
+                  <Badge className="decaf-company__field" color="info">Aerospace</Badge> 
+                </div>
+                <div className="decaf-company__nationalities">
+                  <Badge className="decaf-company__nationality" color="secondary">US Citizen</Badge>
+                  <Badge className="decaf-company__nationality" color="secondary">International</Badge>
+                </div>
+                <CardText cla>This is some text about the company</CardText>
               </CardBody>
             </Card>
+            <Card className="decaf-company">
+              <CardBody>
+                <CardTitle className="decaf-company__name">Astranis</CardTitle>
+                <CardSubtitle className="decaf-compay__position">Full-Time, Internship</CardSubtitle>
+                <div className="decaf-company__fields">
+                  <Badge className="decaf-company__field" color="info">Mechanical</Badge>
+                  <Badge className="decaf-company__field" color="info">Aerospace</Badge>
+                  <Badge className="decaf-company__field" color="info">Aerospace</Badge> 
+                </div>
+                <div className="decaf-company__nationalities">
+                  <Badge className="decaf-company__nationality" color="secondary">US Citizen</Badge>
+                  <Badge className="decaf-company__nationality" color="secondary">International</Badge>
+                </div>
+                <CardText cla>This is some text about the company</CardText>
+              </CardBody>
+            </Card>
+            
           </CardDeck>
         </div>
       </div>
--- src/pages/DecafPage/components/Company.js
+++ src/pages/DecafPage/components/Company.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { Card, CardTitle, CardSubtitle } from 'reactstrap';
+import { Card, CardBody, CardTitle, CardText, Badge, CardSubtitle } from 'reactstrap';
 
 const Company = (props) => (
-  <Card className='company'>
-    <CardTitle className='company__name'></CardTitle>
-    <CardSubtitle className='company__position'></CardSubtitle>
+  <Card className="decaf-company">
+    <CardBody>
+      <CardTitle className="decaf-company__name">{ props.name }</CardTitle>
+      <CardSubtitle className="decaf-company__positions">{ props.positions.toString().replace(/, /g, ",").replace(/,/g, ", ") }</CardSubtitle>
+      <div className="decaf-company__fields">
+        {
+          props.fields.map((field) => <Badge className="decaf-company__field" color="info">{field}</Badge>)
+        }
+      </div>
+      <div className="decaf-company__nationalities">
+        {
+          props.nationalities.map((nationality) => <Badge className="decaf-company__nationality" color="secondary">{nationality}</Badge>)
+        }
+      </div>
+      <CardText cla>{props.description}</CardText>
+    </CardBody>
   </Card>
 )
 
