d4fe948ab7ba840ef70593497eec1be9129cb236
Saurabh Kanhegaonkar <saurabh.kanhegaonkar@gmail.com>
2019-01-10 02:01:18 -0800

    Added avatar bubbles for positions

--- src/pages/DecafPage/components/Company.js
+++ src/pages/DecafPage/components/Company.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Card, CardBody, CardTitle, CardText, Badge, CardSubtitle } from 'reactstrap';
+import { Card, CardBody, CardTitle, CardText, Badge, CardSubtitle, CardFooter } from 'reactstrap';
+import Avatar from 'react-avatar';
 
 const Company = (props) => (
   <Card className="decaf-company">
     <CardBody>
       <CardTitle className="decaf-company__name">{ props.name }</CardTitle>
-      <CardSubtitle className="decaf-company__positions">{ props.positions.toString().replace(/, /g, ",").replace(/,/g, ", ") }</CardSubtitle>
+      {/* <CardSubtitle className="decaf-company__positions">{ props.positions.toString().replace(/, /g, ",").replace(/,/g, ", ") }</CardSubtitle> */}
       <div className="decaf-company__fields">
           <Badge className="decaf-company__field" color="info">{props.field}</Badge>
       </div>
@@ -16,6 +17,11 @@ const Company = (props) => (
       </div>
       <CardText>{props.description}</CardText>
     </CardBody>
+    <CardFooter className="decaf-company__footer">
+        {
+          props.positions.map(position => <Avatar name={position} size={25} round={true} textSizeRatio="2.00" className="decaf-company__avatar" />)
+        }
+    </CardFooter>
   </Card>
 )
 
