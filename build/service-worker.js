b37f4a9eb0e9d75279dd176f55d9420e77f3789b
Lisa Luo <llluo@ucsd.edu>
2019-09-21 11:02:11 -0700

    npm run build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9ba2c9159b430ca084937ae9361c0db4.js"
+  "/precache-manifest.aff48aefd8c5f370624d021fef620bf6.js"
 );
 
 workbox.clientsClaim();
