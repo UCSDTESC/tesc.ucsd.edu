f202c9b6d31559ed568d595d9f615281493e1273
Lisa Luo <llluo@ucsd.edu>
2019-09-21 23:12:35 -0700

    added fb event button to eotg

--- src/pages/EOTGPage/components/Hero.js
+++ src/pages/EOTGPage/components/Hero.js
@@ -18,48 +18,10 @@ class Hero extends React.Component {
     render() {
         return (
             <section className="w-100 h-100 d-flex justify-content-center eotg-hero">
-                {/*<div>
-            <nav className="navbar navbar-expand-lg navbar-light nav__items sticky-top eotg-nav">
-                {/*<NavLink to="/decaf"><img className="nav__logo w-50" href="#" src="/decaf-icon.svg" /></NavLink>
-                <button className="navbar-toggler nav__toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon" />
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item nav__item text-center">
-                            <NavLink className="nav-link text-white" to="/">HOME</NavLink>
-                        </li>
-                        <li className="nav-item nav__item text-center">
-                            <NavLink className="nav-link text-white" to="/decaf#about">ABOUT</NavLink>
-                        </li>
-                        <li className="nav-item nav__item text-center">
-                            <NavLink className="nav-link text-white" to="/decaf#attending">ATTENDING</NavLink>
-                        </li>
-                        <li className="nav-item text-center">
-                            <NavLink className="nav-link text-white" to="/decaf#flo">FLO</NavLink>
-                        </li>
-                        <li className="nav-item text-center">
-                            <NavLink className="nav-link text-white" to="/decaf#faq">FAQ</NavLink>
-                        </li>
-                        <li className="nav-item nav__item text-center">
-                            <NavLink className="nav-link text-white" to="/decaf#contact">CONTACT</NavLink>
-                        </li>
-                        <li className="nav-item text-center">
-                            <NavLink className="nav-link btn nav__toggle decaf-toggle" to="/decaf/companies">FOR COMPANIES</NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            </div> */}
-
-
                 <div className="container-fluid text-center eotg-contents">
-                    {/*<Nav />*/}
                     <div className="row h-100">
-                        
                         <div className="col-md-6">
                             <HeroPic className="eotg-hero__graphic" />
-                            {/*<img src="../svg/logo.svg" />*/}
                         </div>
                         <div className="col-md-4 d-flex align-items-center eotg-hero__left">
                             <div>
@@ -71,6 +33,9 @@ class Hero extends React.Component {
                                 </div>
                                 <div className="eotg-hero__detail">
                                     September 30, 2019 // 2PM - 6PM
+                                </div>
+                                <div className="eotg-hero__button_div">
+                                    <a className="eotg-hero__button" href="https://www.facebook.com/events/341443063439848/" target="_blank">Facebook Event Page</a>
                                 </div>
                             </div>
                         </div>
