3e069ae7df8eb5da71fccd34aebb5891a82df751
Subhankar Panda <s1panda@ucsd.edu>
2020-05-11 20:22:37 -0400

    Added We Empower Animation

--- src/pages/HomePage/components/Hero.js
+++ src/pages/HomePage/components/Hero.js
@@ -1,39 +1,32 @@
 import React from 'react';
+import WeEmpower from './WeEmpower';
 
-class Hero extends React.Component {
+function Hero() {
 
-    componentDidMount() {
-        /*let tl = new TimelineMax();
-        tl.staggerFrom('#laptop_text_copy_2 > g > g path', 0.1, {scaleX: 0}, 0.1)
-            .from('#XMLID_123_', 1, {scaleY: 0, transformOrigin: "50% 50%", ease: Power2.easeOut});*/
-    }
-
-    render() {
-        return (
-            <section className="hero">
-                <div className="container-fluid p-0 h-100 mx-0">
-                    <div className="row h-100 hero__row w-100 mx-0">
-                        <div className="col-md-6 d-flex">
-                            <div className="align-self-center hero__fix">
-                            <div className="hero__copy">
-                                We empower engineers.
-                            </div>
-                            <div className="hero__cta">
-                                <a className="btn hero__cta-btn text-white" href="http://eepurl.com/gFrQpH" target="_blank"> Subscribe <i className="fa fa-envelope" /> </a>
-                            </div>
-                            </div>
+    return (
+        <section className="hero">
+            <div className="container-fluid p-0 h-100 mx-0">
+                <div className="row h-100 hero__row w-100 mx-0">
+                    <div className="col-md-6 d-flex">
+                        <div className="align-self-center hero__fix">
+                        <div className="hero__copy">
+                            <WeEmpower />
                         </div>
-                        <div className="col-md-6 p-0 d-flex">
-                            {/*<div className="hero__graphic w-100">
-                                <HeroGraphic className="w-100"/>
-                            </div>*/}
-                            <img className="hero__graphic img-fluid" src="/hero.png"/>
+                        <div className="hero__cta">
+                            <a className="btn hero__cta-btn text-white" href="https://tesc.typeform.com/to/bAqdgm" target="_blank"> Join Us <i class="fas fa-external-link-square-alt"></i> </a>
+                        </div>
                         </div>
                     </div>
+                    <div className="col-md-6 p-0 d-flex">
+                        {/*<div className="hero__graphic w-100">
+                            <HeroGraphic className="w-100"/>
+                        </div>*/}
+                        <img className="hero__graphic img-fluid" src="/hero.png"/>
+                    </div>
                 </div>
-            </section>
-        );
-    }
+            </div>
+        </section>
+    );
 }
 
 export default Hero;
