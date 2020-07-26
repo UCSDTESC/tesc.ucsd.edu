fef3b4a2eb527ae41484c04e3072b99a1dd44cb4
Lisa Luo <llluo@ucsd.edu>
2020-07-25 19:45:54 -0700

    change script

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/expect -f
+set password [lindex $argv 0]
+
 spawn rsync -avzr --delete -e "ssh -i ~/.ssh/gh-actions-key -o StrictHostKeyChecking=no" "./build/" tesc@tesc.ucsd.edu:~/htdocs
 expect "tesc@tesc.ucsd.edu's password:"
-send $1
+send "$password\r"
