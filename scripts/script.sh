a8cb5ab327232eeeae99ac99d2c9b45d616c5e01
Lisa Luo <llluo@ucsd.edu>
2020-07-25 19:22:49 -0700

    remove quotes from script

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/expect -f
-spawn "rsync -avzr --delete -e 'ssh -i ~/.ssh/gh-actions-key -o StrictHostKeyChecking=no' './build/' tesc@tesc.ucsd.edu:~/htdocs"
+spawn rsync -avzr --delete -e "ssh -i ~/.ssh/gh-actions-key -o StrictHostKeyChecking=no" "./build/" tesc@tesc.ucsd.edu:~/htdocs
 expect "tesc@tesc.ucsd.edu's password:"
 send ${{ secrets.SSH_PASSWORD }}
