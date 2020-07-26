b8cfa18eb141e5f94e10060904b0b164c88baa87
Lisa Luo <llluo@ucsd.edu>
2020-07-25 20:24:11 -0700

    add yes

--- scripts/script.sh
+++ scripts/script.sh
@@ -3,6 +3,9 @@ set timeout -1
 set password [lindex $argv 0]
 
 spawn rsync -avzr --delete -e ssh ./build/ tesc@tesc.ucsd.edu:~/htdocs
+expect "(yes/no)?"
+send "yes\r"
+
 expect "password:"
 send "$password\r"
 
