5b675ac7b129f8e1f4eb0de828e1de9463446635
Lisa Luo <llluo@ucsd.edu>
2020-07-25 19:50:26 -0700

    add done message

--- scripts/script.sh
+++ scripts/script.sh
@@ -4,3 +4,5 @@ set password [lindex $argv 0]
 spawn rsync -avzr --delete -e "ssh -i ~/.ssh/gh-actions-key -o StrictHostKeyChecking=no" "./build/" tesc@tesc.ucsd.edu:~/htdocs
 expect "tesc@tesc.ucsd.edu's password:"
 send "$password\r"
+
+puts "i guess it's done"
