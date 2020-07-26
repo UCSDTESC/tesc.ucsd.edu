448e54a41f6d0185a2db64c2d66bbbc582332106
Subhankar Panda <s1panda@ucsd.edu>
2020-07-25 20:05:44 -0400

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/expect -f
+ls
+echo ${{ secrets.SSH_PASSWORD }}
 spawn "rsync -avzr --delete -e 'ssh -i ~/.ssh/gh-actions-key -o StrictHostKeyChecking=no' './build/' tesc@tesc.ucsd.edu:~/htdocs"
 expect "tesc@tesc.ucsd.edu's password:"
 send ${{ secrets.SSH_PASSWORD }}
