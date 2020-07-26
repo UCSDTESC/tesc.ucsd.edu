4a28b8342553db9191279b2c69316b2f29e5fc79
Lisa Luo <llluo@ucsd.edu>
2020-07-25 20:09:57 -0700

    try just password

--- scripts/script.sh
+++ scripts/script.sh
@@ -3,7 +3,7 @@ set timeout 60
 set password [lindex $argv 0]
 
 spawn rsync -avzr --delete -e "ssh -i ~/.ssh/gh-actions-key -o StrictHostKeyChecking=no" "./build/" tesc@tesc.ucsd.edu:~/htdocs
-expect "tesc@tesc.ucsd.edu's password:"
+expect "password:"
 send "$password\r"
 
 puts "i guess it's done"
