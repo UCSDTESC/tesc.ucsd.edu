10562bbf8f04952e510597a438ea3e44c988ec19
Lisa Luo <llluo@ucsd.edu>
2020-07-25 20:46:50 -0700

    add progress flag and use absolute path

--- scripts/script.sh
+++ scripts/script.sh
@@ -2,7 +2,7 @@
 set timeout -1
 set password [lindex $argv 0]
 
-spawn rsync -avzr --delete -e ssh ./build/ tesc@tesc.ucsd.edu:~/htdocs/
+spawn rsync -avzr --progress --delete -e ssh ./build/ tesc@tesc.ucsd.edu:/var/www/tesc/htdocs/
 expect "(yes/no)?"
 send "yes\r"
 
