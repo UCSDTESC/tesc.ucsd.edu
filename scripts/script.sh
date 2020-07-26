38da63bf4fc696d3dd9c3f8169faf06074637fb5
Lisa Luo <llluo@ucsd.edu>
2020-07-25 19:15:19 -0700

    remove ls

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/expect -f
-ls
-# echo ${{ secrets.SERVER_DESTINATION }}
 spawn "rsync -avzr --delete -e 'ssh -i ~/.ssh/gh-actions-key -o StrictHostKeyChecking=no' './build/' tesc@tesc.ucsd.edu:~/htdocs"
 expect "tesc@tesc.ucsd.edu's password:"
 send ${{ secrets.SSH_PASSWORD }}
