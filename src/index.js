1855a62b338b3895ec4a91fe4d24e66ffa0a9671
Kenneth Chen <efeichen@gmail.com>
2020-08-22 15:02:20 -0700

    Replace prebuild bundle with ES6 package

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
 import './scss/tesc-website.scss';
 import './globals';
-import './fullcalendar.min.css';
-import './gcal';
 
-ReactDOM.render((
+ReactDOM.render(
     <BrowserRouter>
         <Routes />
-    </BrowserRouter>
-), document.getElementById('app'));
+    </BrowserRouter>,
+    document.getElementById('app')
+);
