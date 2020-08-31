8d2a689c70a619d3cc5dc4bee36e3901711879bd
Kenneth Chen <efeichen@gmail.com>
2020-08-30 23:01:02 -0700

    Add new hero graphic

--- src/pages/EOTGPage/components/Hero.js
+++ src/pages/EOTGPage/components/Hero.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import $ from 'jquery';
-import {ReactComponent as HeroPic} from '../svg/eotg-wi20.svg';
+import { ReactComponent as HeroPic } from '../svg/eotg-f20.svg';
 
 class Hero extends React.Component {
-
     scrollAnimation() {
-        $('html, body').animate({
-            scrollTop: $('#about').offset().top
-        }, 800);
+        $('html, body').animate(
+            {
+                scrollTop: $('#about').offset().top,
+            },
+            800
+        );
     }
 
     render() {
@@ -15,24 +17,34 @@ class Hero extends React.Component {
             <section className="w-100 h-100 d-flex justify-content-center eotg-hero">
                 <div className="container-fluid text-center eotg-hero__container">
                     <div className="row h-100 justify-content-center">
-                        <div className="col-md-6">
+                        <div className="col-md-6 d-flex align-items-center">
                             <HeroPic className="eotg-hero__graphic" />
                         </div>
                         <div className="col-md-6 d-flex align-items-center eotg-hero__right">
                             <div>
                                 <div className="eotg-hero__title pb-2">
-                                <b> Engineers on the Green </b>
+                                    <b> Engineers on the Green </b>
                                 </div>
                                 <div className="eotg-hero__detail">
                                     October 5th - 9th // 2-6 PM each day
                                 </div>
                                 <div className="eotg-hero__button_div mb-5">
-                                    <a className="eotg-hero__button" href="https://www.facebook.com/events/638144703462548/" target="_blank" rel="noopener noreferrer">
+                                    <a
+                                        className="eotg-hero__button"
+                                        href="https://www.facebook.com/events/638144703462548/"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
                                         Facebook Event Page
                                     </a>
                                 </div>
                                 <div className="eotg-hero__button_div">
-                                    <a className="eotg-hero__button" href="https://discord.gg/WBaecP2" target="_blank" rel="noopener noreferrer">
+                                    <a
+                                        className="eotg-hero__button"
+                                        href="https://discord.gg/WBaecP2"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
                                         Discord
                                     </a>
                                 </div>
@@ -41,7 +53,7 @@ class Hero extends React.Component {
                     </div>
                 </div>
             </section>
-        )
+        );
     }
 }
 
