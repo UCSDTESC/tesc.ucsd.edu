8442e981529d7636e8e00f370d3772f86efa07b7
Kenneth Chen <efeichen@gmail.com>
2020-09-04 12:04:27 -0700

    Update events

--- src/pages/HomePage/components/Events.js
+++ src/pages/HomePage/components/Events.js
@@ -8,12 +8,16 @@ function Event(props) {
     let withWrapper = null;
     if (props.isInternalLink) {
         withWrapper = (jsx) => {
-            return <Link to={props["Link"]}>{jsx}</Link>;
+            return <Link to={props['Link']}>{jsx}</Link>;
         };
     } else {
         withWrapper = (jsx) => {
             return (
-                <a href={props["Link"]} target="_blank" rel="noopener noreferrer">
+                <a
+                    href={props['Link']}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     {jsx}
                 </a>
             );
@@ -22,11 +26,21 @@ function Event(props) {
     return (
         <div className="col-md-3 align-self-center d-flex flex-column p-3 events__card">
             {/* attach link to image and print name/dates */}
-            {props["Event Name"]}
-            {withWrapper(<img className="align-self-center justify-self-center" alt={props["Event Name"]} 
-                    src={props["Image"] !== undefined && props["Image"][0].url} style={{maxHeight: "8rem", width: "auto", maxWidth: "100%"}}/>)}
-            
-            {moment(props["Start Date"]).format('MMMM Do YYYY')}
+            {props['Event Name']}
+            {withWrapper(
+                <img
+                    className="align-self-center justify-self-center"
+                    alt={props['Event Name']}
+                    src={props['Image'] !== undefined && props['Image'][0].url}
+                    style={{
+                        maxHeight: '8rem',
+                        width: 'auto',
+                        maxWidth: '100%',
+                    }}
+                />
+            )}
+
+            {moment(props['Start Date']).format('MMMM Do YYYY')}
         </div>
     );
 }
@@ -42,10 +56,14 @@ function Events() {
         base.current('Council Org Events')
             .select()
             .all()
-            .then(records => records.map(r => r._rawJson.fields))
-            .then(records => records.filter(r => r.Validated))
-            .then(records => records.sort((a, b) => {return (new Date(a["Start Date"])- new Date(b["End Date"]))}))
-            .then(records => setRecords(records))
+            .then((records) => records.map((r) => r._rawJson.fields))
+            .then((records) => records.filter((r) => r.Validated))
+            .then((records) =>
+                records.sort((a, b) => {
+                    return new Date(a['Start Date']) - new Date(b['End Date']);
+                })
+            )
+            .then((records) => setRecords(records));
     }, []);
 
     // render current events by creating event cards
@@ -58,11 +76,16 @@ function Events() {
                 </div>
             );
         }
-        currEvents = currEvents.filter((event) => {
-            return ((new Date(event["Start Date"]) - new Date())>0);
-        }).sort((event, event2) => {
-            return ((new Date(event["Start Date"]) - new Date(event2["Start Date"])));
-        });
+        currEvents = currEvents
+            .filter((event) => {
+                return new Date(event['Start Date']) - new Date() > 0;
+            })
+            .sort((event, event2) => {
+                return (
+                    new Date(event['Start Date']) -
+                    new Date(event2['Start Date'])
+                );
+            });
         return currEvents.map((c, i) => <Event {...c} key={i} />);
     }
 
@@ -77,30 +100,26 @@ function Events() {
             );
         }
         pastEvents = pastEvents.filter((event) => {
-            return ((new Date(event["Start Date"]) - new Date())<0);
+            return new Date(event['Start Date']) - new Date() < 0;
         });
         // limit number of past events to max 5
         return pastEvents.slice(0, 5).map((c, i) => <Event {...c} key={i} />);
     }
     return (
-            <section className="events mb-5" id="events">
-                <div className="text-center events__red">What's Happening?</div>
-                <div className="text-center events__header">
-                    Upcoming Events
+        <section className="events mb-5" id="events">
+            <div className="text-center events__red">What's Happening?</div>
+            <div className="text-center events__header">Upcoming Events</div>
+            <div className="container events__container">
+                <div className="row justify-content-center flex-row events__section events__cards">
+                    {renderCurrEvents()}
                 </div>
-                <div className="container events__container">
-                    <div className="row justify-content-center flex-row events__section events__cards">
-                        {renderCurrEvents()}
-                    </div>
-                    <div className="text-center events__header">
-                        Past Events
-                    </div>
-                    <div className="row justify-content-center flex-row events__section events__cards">
-                        {renderPastEvents()}
-                    </div>
+                <div className="text-center events__header">Past Events</div>
+                <div className="row justify-content-center flex-row events__section events__cards">
+                    {renderPastEvents()}
                 </div>
-            </section>
-        );
+            </div>
+        </section>
+    );
 }
 
 export default Events;
