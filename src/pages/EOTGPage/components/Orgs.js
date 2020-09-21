14b018b271b38651862f7c6346d7128e8a5f0e5b
Kenneth Chen <efeichen@gmail.com>
2020-09-21 15:36:58 -0700

    Replace orgs list with Airtable iframe

--- src/pages/EOTGPage/components/Orgs.js
+++ src/pages/EOTGPage/components/Orgs.js
@@ -1,138 +1,101 @@
-import React, { useState } from 'react';
-import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import React from 'react';
 
-import { attendingOrgs } from '../data/Orgs';
-
-// Sort orgs by day
-let sortedOrgs = {};
-function sortOrgs() {
-    attendingOrgs.forEach((org) => {
-        if (!sortedOrgs[org.day]) {
-            sortedOrgs[org.day] = [];
-        }
-        sortedOrgs[org.day].push(org);
-    });
-}
-sortOrgs();
-
-function IndividualOrg(props) {
-    return (
-        <div className="eotg-org__content" onClick={props.toggleModal}>
-            {props.name}
-        </div>
-    );
-}
+import { orgGalleries } from '../data/OrgGalleries';
 
 const Orgs = () => {
-    const [currOrg, setCurrOrg] = useState(null);
-
     // Renders a single day of orgs
-    const renderDay = (orgs) => {
-        return orgs.map((org) => (
-            <IndividualOrg {...org} toggleModal={() => setCurrOrg(org)} />
-        ));
+    const renderDay = (day) => {
+        return (
+            <iframe
+                className="airtable-embed"
+                src={`${orgGalleries[day]}?backgroundColor=green`}
+                title={`orgs on ${day}`}
+                frameBorder="0"
+                width="100%"
+                height="585"
+                style={{ background: 'transparent', border: '1px solid #ccc' }}
+            />
+        );
     };
 
     return (
         <section className="eotg-attending-orgs" id="orgs">
-            {/* Modal that pops up when an org is clicked on */}
-            <Modal
-                isOpen={currOrg != null}
-                centered={true}
-                toggle={() => setCurrOrg(null)}
-            >
-                {currOrg && (
-                    <>
-                        <ModalHeader>{currOrg.name}</ModalHeader>
-                        <ModalBody>
-                            {currOrg.acronym && (
-                                <p>
-                                    <b>Acronym:</b> {currOrg.acronym}
-                                </p>
-                            )}
-                            {currOrg.description && (
-                                <p>
-                                    <b>Description:</b> {currOrg.description}
-                                </p>
-                            )}
-                            {currOrg.department && (
-                                <p>
-                                    <b>Departments:</b> {currOrg.department}
-                                </p>
-                            )}
-                        </ModalBody>
-                        <ModalFooter>
-                            {currOrg.link && (
-                                <a
-                                    className="btn nav__toggle"
-                                    href={currOrg.link}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    <i className="fa fa-link"></i> Website
-                                </a>
-                            )}
-                            {currOrg.email && (
-                                <a
-                                    className="btn nav__toggle"
-                                    href={`mailto:${currOrg.email}`}
-                                    rel="noopener noreferrer"
-                                >
-                                    <i className="fa fa-envelope"></i> Email
-                                </a>
-                            )}
-                        </ModalFooter>
-                    </>
-                )}
-            </Modal>
-
             <h1 className="eotg-attending-orgs__header mb-5">
                 Attending Organizations
             </h1>
-            <div className="container eotg-org">
+            <div className="container">
                 <div className="row justify-content-center eotg-attending-orgs__blurb">
                     This year, Engineers on the Green will take place over five
                     days. Below are the participating organziations grouped by
-                    what day they will be attending. Click on each day to see the
-                    organizations that will be attending, and click on an 
+                    what day they will be attending. Click on each day to see
+                    the organizations that will be attending, and click on an
                     organization's name to learn more.
                 </div>
 
                 <div className="row justify-content-center">
                     {/* The nav bar tabs that display days */}
                     <ul className="nav nav-tabs justify-content-center align-items-end mt-4">
-                        <li className="active"><a data-toggle="tab" href="#monday">Monday, Oct 5th</a></li>
-                        <li><a data-toggle="tab" href="#tuesday">Tuesday, Oct 6th</a></li>
-                        <li><a data-toggle="tab" href="#wednesday">Wednesday, Oct 7th</a></li>
-                        <li><a data-toggle="tab" href="#thursday">Thursday, Oct 8th</a></li>
-                        <li><a data-toggle="tab" href="#friday">Friday, Oct 9th</a></li>
+                        <li className="active">
+                            <a data-toggle="tab" href="#monday">
+                                Monday, Oct 5th
+                            </a>
+                        </li>
+                        <li>
+                            <a data-toggle="tab" href="#tuesday">
+                                Tuesday, Oct 6th
+                            </a>
+                        </li>
+                        <li>
+                            <a data-toggle="tab" href="#wednesday">
+                                Wednesday, Oct 7th
+                            </a>
+                        </li>
+                        <li>
+                            <a data-toggle="tab" href="#thursday">
+                                Thursday, Oct 8th
+                            </a>
+                        </li>
+                        <li>
+                            <a data-toggle="tab" href="#friday">
+                                Friday, Oct 9th
+                            </a>
+                        </li>
                     </ul>
 
                     {/* The info that appears when you click each tab */}
-                    <div class="tab-content">
+                    <div className="tab-content w-100">
                         <div id="monday" className="tab-pane fade show active">
-                            <div className="eotg-attending-orgs__day">Monday, Oct 5th</div>
-                            {renderDay(sortedOrgs['Monday'])}
+                            <div className="eotg-attending-orgs__day">
+                                Monday, Oct 5th
+                            </div>
+                            {renderDay('mon')}
                         </div>
                         <div id="tuesday" className="tab-pane fade">
-                            <div className="eotg-attending-orgs__day">Tuesday, Oct 6th</div>
-                            {renderDay(sortedOrgs['Tuesday'])}
+                            <div className="eotg-attending-orgs__day">
+                                Tuesday, Oct 6th
+                            </div>
+                            {renderDay('tue')}
                         </div>
                         <div id="wednesday" className="tab-pane fade">
-                            <div className="eotg-attending-orgs__day">Wednesday, Oct 7th</div>
-                            {renderDay(sortedOrgs['Wednesday'])}
+                            <div className="eotg-attending-orgs__day">
+                                Wednesday, Oct 7th
+                            </div>
+                            {renderDay('wed')}
                         </div>
                         <div id="thursday" className="tab-pane fade">
-                            <div className="eotg-attending-orgs__day">Thursday, Oct 8th</div>
-                            {renderDay(sortedOrgs['Thursday'])}
+                            <div className="eotg-attending-orgs__day">
+                                Thursday, Oct 8th
+                            </div>
+                            {renderDay('thu')}
                         </div>
                         <div id="friday" className="tab-pane fade">
-                            <div className="eotg-attending-orgs__day">Friday, Oct 9th</div>
-                            {renderDay(sortedOrgs['Friday'])}
+                            <div className="eotg-attending-orgs__day">
+                                Friday, Oct 9th
+                            </div>
+                            {renderDay('fri')}
                         </div>
                     </div>
                 </div>
-                
             </div>
         </section>
     );
