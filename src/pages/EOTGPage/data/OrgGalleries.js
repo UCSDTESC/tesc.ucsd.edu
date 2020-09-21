25c50253c7cecfc3d11b6d9da4f18a02820486c6
Kenneth Chen <efeichen@gmail.com>
2020-09-21 15:36:58 -0700

    Replace orgs list with Airtable iframe

---
+++ src/pages/EOTGPage/data/OrgGalleries.js
@@ -0,0 +1,10 @@
+// map day of week to Airtable gallery view of attending orgs on that day
+const orgGalleries = {
+    mon: 'https://airtable.com/embed/shrwcg7VSGWWOT6OB',
+    tue: 'https://airtable.com/embed/shrERPCjrAFp2OnHf',
+    wed: 'https://airtable.com/embed/shrBAqZE85nKjqA0N',
+    thu: 'https://airtable.com/embed/shrS1ZftBvuLtdtH5',
+    fri: 'https://airtable.com/embed/shrKEfV8QjFxNCeKi',
+};
+
+export { orgGalleries };
