bf4ef3d24e9579d6f0521c0ecbdda9c3ab35ff6b
Elias Fang <56064410+eliasfang@users.noreply.github.com>
2020-11-15 22:17:55 -0800

    add daily rundown section (#54)
    * add daily rundown section
    
    * Add shadow to cards & make responsive
    
    * Fixed 2nd calendar row positioning
    
    Co-authored-by: Kenneth Chen <efeichen@gmail.com>

--- src/pages/MatchaPage/data/MatchaSessions.js
+++ src/pages/MatchaPage/data/MatchaSessions.js
@@ -1,23 +1,23 @@
 const sessionsData = [
     {
         title: 'Resume and Elevator Pitch Sessions',
-        organizer: 'Association for Computing Machinery (ACM)',
+        organizer: 'Association for Computing Machinery (ACM)'
     },
     {
         title: 'Elevator Speech Workshop',
-        organizer: 'American Institute of Aeronautics and Astronautics (AIAA)',
+        organizer: 'American Institute of Aeronautics and Astronautics (AIAA)'
     },
     {
         title: 'How to Share Technical Project Experience with Recruiters',
-        organizer: 'Rocket Propulsion Laboratory (RPL)',
+        organizer: 'Rocket Propulsion Laboratory (RPL)'
     },
     {
         title: 'What to Do After an Engineering Conference',
-        organizer: 'Institute of Electrical and Electronics Engineers (IEEE)',
+        organizer: 'Institute of Electrical and Electronics Engineers (IEEE)'
     },
     {
         title: 'Interview Insights and Preparation Workshop',
-        organizer: 'Engineers Without Borders (EWB)',
+        organizer: 'Engineers Without Borders (EWB)'
     },
 ];
 
