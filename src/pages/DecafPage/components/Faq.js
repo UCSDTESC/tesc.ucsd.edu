8d91837b40ca0fcf791f84b70b10cefec93ea5c5
LiLronV <36945156+LiLronV@users.noreply.github.com>
2021-01-09 15:12:40 -0800

    Created Faq section (Company)

--- src/pages/DecafPage/components/Faq.js
+++ src/pages/DecafPage/components/Faq.js
@@ -4,9 +4,11 @@ import { Container, Row, Col } from 'reactstrap';
 import BorderTop from '../assets/border-faq-top.svg';
 import BorderBottom from '../assets/border-faq-bottom.svg';
 import Spotlight from '../assets/spotlight.svg';
-
+import studentFaq from '../data/DecafFaq';
+import FaqItem from './FaqItem';
 const Faq = () => {
     return (
+        
         <section className="decaf-faq">
             <img src={BorderTop} alt="" className="decaf-faq__border-top" />
             <img src={Spotlight} alt="" />
@@ -16,6 +18,18 @@ const Faq = () => {
                     <br />
                     <em>QUESTIONS</em>
                 </h1>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12 my-auto">
+                            <div className="content pb-4">
+                                {studentFaq.map((d, i) => (
+                                    <FaqItem {...d} key={i} idx={i} />
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </Container>
             <img
                 src={BorderBottom}
