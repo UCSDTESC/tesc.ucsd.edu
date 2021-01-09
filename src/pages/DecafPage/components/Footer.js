0d7fa2019ef0c3d003961a34a3d9c9cee9ddc8cb
Elias Fang <eliasfang@gmail.com>
2021-01-09 11:58:05 -0800

    Format footer and make responsive

--- src/pages/DecafPage/components/Footer.js
+++ src/pages/DecafPage/components/Footer.js
@@ -7,9 +7,9 @@ const Footer = () => {
     return (
         <footer className="decaf-footer">
             <Container>
-                <p>organized by</p>
-                <img src={TescLogo} alt="TESC logo" />
-                <p>
+                <p className="decaf-subtitle">organized by</p>
+                <img className="decaf-tesc" src={TescLogo} alt="TESC logo" />
+                <p className="decaf-desc">
                     The Triton Engineering Student Council empowers UC San Diego
                     engineering students by connecting them with impactful
                     communities, projects, and career building opportunities.
