824a3f9fffc27edf1b8795bd175359741798c459
Kenneth Chen <efeichen@gmail.com>
2021-01-14 17:14:14 -0800

    Update Enspire favicon & event cards

--- src/pages/EnspirePage/components/Register.js
+++ src/pages/EnspirePage/components/Register.js
@@ -10,7 +10,7 @@ const Register = () => {
             <Container>
                 <div className="enspire-register__title">
                     <h2 className="sup">held virtually on</h2>
-                    <h1 className="main">January 23, 2021</h1>
+                    <h1 className="main">January 30, 2021</h1>
                     <h2 className="sub">9-3PM PST</h2>
                 </div>
                 {/* <Button className="d-block" href="">
--- src/pages/EnspirePage/index.js
+++ src/pages/EnspirePage/index.js
@@ -9,10 +9,7 @@ import Faq from './components/Faq';
 const EnspirePage = () => {
     useEffect(() => {
         document.title = 'Enspire 2021 | UCSD TESC';
-        $("link[rel='shortcut icon']").attr(
-            'href',
-            'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚙️</text></svg>'
-        );
+        $("link[rel='shortcut icon']").attr('href', '/enspire-favicon.svg');
 
         return () => {
             document.title = 'UCSD TESC';
