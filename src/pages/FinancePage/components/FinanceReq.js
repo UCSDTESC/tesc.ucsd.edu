cac93ed59b06689b8f338a216da096836a47114f
Kenneth Chen <efeichen@gmail.com>
2021-01-23 21:24:42 -0800

    Update finance page

--- src/pages/FinancePage/components/FinanceReq.js
+++ src/pages/FinancePage/components/FinanceReq.js
@@ -1,26 +1,21 @@
 import React from 'react';
 
 class FinanceReq extends React.Component {
-
-
     render() {
-        const {data} = this.props;
-        if (data.title === 'Funding Rules' || data.title === 'What’s Required') {
+        const { data } = this.props;
+        if (
+            data.title === 'Funding Rules' ||
+            data.title === 'What’s Required'
+        ) {
             return (
                 <div className="req__item">
-                    <div className="d-flex" >
-                        <div className="req__item-header ">
-                            {data.title}
-                        </div>
+                    <div className="d-flex">
+                        <div className="req__item-header ">{data.title}</div>
                     </div>
                     <div className="req__item-content px-5 py-3">
                         <ul>
-                            {data.content.map((c,i) => {
-                                return (
-                                    <li key={i}>
-                                        {c}
-                                    </li>
-                                );
+                            {data.content.map((c, i) => {
+                                return <li key={i}>{c}</li>;
                             })}
                         </ul>
                     </div>
@@ -29,19 +24,13 @@ class FinanceReq extends React.Component {
         } else {
             return (
                 <div className="req__item">
-                    <div className="d-flex" >
-                        <div className="req__item-header ">
-                            {data.title}
-                        </div>
+                    <div className="d-flex">
+                        <div className="req__item-header ">{data.title}</div>
                     </div>
                     <div className="req__item-content px-5 py-3">
                         <ol>
-                            {data.content.map((c,i) => {
-                                return (
-                                    <li key={i}>
-                                        {c}
-                                    </li>
-                                );
+                            {data.content.map((c, i) => {
+                                return <li key={i}>{c}</li>;
                             })}
                         </ol>
                     </div>
