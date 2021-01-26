455b4bc7e69a3a0a8e8bd0054fe23754a889ca50
Kenneth Chen <efeichen@gmail.com>
2021-01-25 18:31:09 -0800

    Add jellyfish graphics

--- src/pages/EnspirePage/components/About.js
+++ src/pages/EnspirePage/components/About.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { Container } from 'reactstrap';
 import Divider from './Divider';
 
+import JellyLeft from '../static/jelly-about-left.svg';
+import JellyRight from '../static/jelly-about-right.svg';
+
 const About = () => {
     return (
         <section className="enspire-about">
+            <img src={JellyLeft} alt="" className="jelly-left" />
+            <img src={JellyRight} alt="" className="jelly-right" />
             <h1 className="section-title">about</h1>
             <Divider />
             <Container>
