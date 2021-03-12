b4ada87988c508975734af27acaf9ca6547e31c0
Elias Fang <eliasfang@gmail.com>
2021-03-12 10:09:46 -0800

    Set up FAQ page

---
+++ src/pages/FAQPage/components/FAQBlurb.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function FAQBlurb() {
+    return (
+        <h1>hello</h1>
+    );
+}
+
+export default FAQBlurb;
---
+++ src/pages/FAQPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FAQBlurb from './components/FAQBlurb';
+
+function FAQPage() {
+    return (
+        <>
+            <FAQBlurb />
+        </>
+    );
+}
+
+export default FAQPage;
