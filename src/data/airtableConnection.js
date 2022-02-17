c180ac07a141548516c9817ee012d247caf7cccb
John Zhou <jozhou@ucsd.edu>
2022-02-17 13:59:45 -0800

    Added dates to the end of about blurb

--- src/data/airtableConnection.js
+++
@@ -1,3 +0,0 @@
-import Airtable from 'airtable';
-
-export default new Airtable({ apiKey: process.env.REACT_APP_AIRTABLE_KEY });
--- src/pages/DecafPage/AttendingPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AttendingCompanies from './components/AttendingCompanies';
-
-const AttendingPage = () => {
-    return <AttendingCompanies />;
-};
-
-export default AttendingPage;
